9f10d4ce0167e5285371ef20a6fe6b84ee7e5261
Erick Rosete Beas <erickrosetebeas@hotmail.com>
2019-04-06 17:02:24 -0700

    Added JQuery Draggable features

--- js/playground.js
+++ js/playground.js
@@ -1,3 +1,38 @@
-window.onload = function () {
-    console.log(document.getElementById("option-1"))
-};
+$(function () {
+    $(".cont-option").draggable({
+        containment: 'document',
+        revert: "invalid",
+        // snap: ".target-option",
+        // snapMode: "outer",
+        // snapTolerance: 30,
+        helper: 'clone',
+        connectToSortable: "#target"
+    });
+
+    $("#target").droppable({
+        hoverClass: 'onHover',
+        accept: '.cont-option',
+        drop: (event, ui) => {
+            const droppedItem = $(ui.draggable);
+            droppedItem.removeClass("cont-option");
+            droppedItem.addClass("target-option");
+
+            // const droppedItem = $(ui.draggable).clone();
+            // droppedItem.removeClass("cont-option");
+            // droppedItem.addClass("target-option");
+            // $(event.target).append(droppedItem)
+        }
+    })
+
+    $("#target").sortable({
+        revert: true
+    });
+
+    $("#trash").droppable({
+        accept: '.target-option',
+        hoverClass: 'onHover',
+        drop: (event, ui) => {
+            $(ui.draggable).remove();
+        }
+    })
+});
