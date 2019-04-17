72c8c3dad93e078333f0b75a2a9ebcd6af05151d
M C <mariela.castillo.mc2@gmail.com>
2019-04-17 10:50:45 -0700

    Graph1

--- index.js
+++ index.js
@@ -41,7 +41,7 @@ setInterval(function(){
   var randy0=String(Math.random()).slice(0,5);
   var randy1=String(Math.random()).slice(0,5);
   var randy2=String(Math.random()).slice(0,5);
-  console.log(randy0);
+//  console.log(randy0);
   mainWindow.webContents.send("Datos", "A"+randy0+",B"+randy1+",C"+randy2);
 },1000);
 
