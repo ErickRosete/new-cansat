2f502a0e0a61a72f5a58a959e9d4ef5db93b51c2
M C <mariela.castillo.mc2@gmail.com>
2019-04-17 19:08:01 -0700

    Mac menuTemplate changes

--- index.js
+++ index.js
@@ -9,7 +9,6 @@ app.on("ready", () => {
   mainWindow = new BrowserWindow({ width: 960, height: 600, fullscreen: false });
   mainWindow.loadFile("./views/playground.html");
   mainWindow.on("closed", () => app.quit());
-  mainWindow.webContents.openDevTools();
 
   const mainMenu = Menu.buildFromTemplate(menuTemplate);
   Menu.setApplicationMenu(mainMenu);
@@ -45,6 +44,7 @@ setInterval(function(){
   mainWindow.webContents.send("Datos", "A"+randy0+",B"+randy1+",C"+randy2);
 },1000);
 
+
 const menuTemplate = [
   {
     label: "CanSat",
@@ -72,11 +72,6 @@ const menuTemplate = [
   }
 ];
 
-//ios
-if (process.platform === "darwin") {
-  menuTemplate.unshift({});
-}
-
 //dev tools
 if (process.env.NODE_ENV !== "production") {
   menuTemplate.push({
@@ -96,3 +91,9 @@ if (process.env.NODE_ENV !== "production") {
     ]
   });
 }
+
+//ios
+if (process.platform === "darwin") {
+ // menuTemplate.unshift({});
+}
+
